5b9b45a90322e22320aa1b204e1aa793d82fc233
Espoir M <espoir.mur@gmail.com>
2017-01-26 15:30:31 +0200

    Client Dashboard patialy finished just remaining upadate profile client Dashboard finished and he can easily  update their profile configurations before first deployement adding Production Configurations

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from app import create_app
 
 config_name = os.environ.get('FLASK_CONFIG')
 if config_name is None:
-    app = create_app("development")
+    app = create_app("developement")
 else:
     app = create_app(config_name)
 if __name__ == '__main__':
