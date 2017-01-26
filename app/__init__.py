e097bd40dcad663952e3a64a0f3270292f641896
Espoir M <espoir.mur@gmail.com>
2017-01-26 13:47:01 +0200

    Client Dashboard patialy finished just remaining upadate profile client Dashboard finished and he can easily  update their profile configurations before first deployement adding Production Configurations

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
-import smtplib
+import smtplib,os
 db = SQLAlchemy() #initialisation de la BD
 log_manager =LoginManager()
 mail = Mail()
@@ -14,40 +14,52 @@ smtpserver.ehlo()
 smtpserver.starttls()"""
 def create_app(config_name):
     #application factory
-    app = Flask(__name__,instance_relative_config=True)
-    app.config.from_pyfile('config.py')
-    app.config.from_object(app_config[config_name])
-    db.init_app(app)
-    mail.init_app(app) #Pour l'envoi des mails
-    log_manager.init_app(app)
-    log_manager.login_message="You must be logged to see this page"
-    log_manager.login_view = "auth.login" #la vue qui gere les login
-    migrate = Migrate(app,db) #Migrate object to databases
-    Bootstrap(app)
-    from app import models
-    from .admin import admin as admin_blueprint
-    app.register_blueprint(admin_blueprint,url_prefix='/admin')
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-    from .home import home as home_blueprint
-    app.register_blueprint(home_blueprint)
-    from .client import client as client_blueprint
-    app.register_blueprint(client_blueprint,url_prefix='/client')
-    from .engineer import eng as eng_blueprint
-    app.register_blueprint(eng_blueprint,url_prefix='/eng')
-    #for error handling
-    @app.errorhandler(403)
-    def forbidden(error):
-        return render_template('errors/403.html', title='Forbidden'), 403
 
-    @app.errorhandler(404)
-    def page_not_found(error):
-        return render_template('errors/404.html', title='Page Not Found'), 404
+    def create_app(config_name):
+        if os.getenv('FLASK_CONFIG') == "production":
+            app = Flask(__name__)
+            app.config.update(
+                SECRET_KEY=os.getenv('SECRET_KEY'),
+                SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+            )
+        else:
+            app = Flask(__name__, instance_relative_config=True)
+            app.config.from_object(app_config[config_name])
+            app.config.from_pyfile('config.py')
+        db.init_app(app)
+        mail.init_app(app)  # Pour l'envoi des mails
+        log_manager.init_app(app)
+        log_manager.login_message = "You must be logged to see this page"
+        log_manager.login_view = "auth.login"  # la vue qui gere les login
+        migrate = Migrate(app, db)  # Migrate object to databases
+        Bootstrap(app)
+        from app import models
+        from .admin import admin as admin_blueprint
+        app.register_blueprint(admin_blueprint, url_prefix='/admin')
+        from .auth import auth as auth_blueprint
+        app.register_blueprint(auth_blueprint)
+        from .home import home as home_blueprint
+        app.register_blueprint(home_blueprint)
+        from .client import client as client_blueprint
+        app.register_blueprint(client_blueprint, url_prefix='/client')
+        from .engineer import eng as eng_blueprint
+        app.register_blueprint(eng_blueprint, url_prefix='/eng')
 
-    @app.errorhandler(500)
-    def internal_server_error(error):
-        return render_template('errors/500.html', title='Server Error'), 500
-    return app
+        # for error handling
+        @app.errorhandler(403)
+        def forbidden(error):
+            return render_template('errors/403.html', title='Forbidden'), 403
+
+        @app.errorhandler(404)
+        def page_not_found(error):
+            return render_template('errors/404.html', title='Page Not Found'), 404
+
+        @app.errorhandler(500)
+        def internal_server_error(error):
+            return render_template('errors/500.html', title='Server Error'), 500
+
+        return app
+
 
 
 
